7dbff68512c92391cf1da938d84e2a77cff10163
cao xinyan <840792248@qq.com>
2016-06-16 16:41:20 +0800

    code

---
+++ src/main/java/cao/crawler/storm_crawler/CrawlTopology.java
@@ -0,0 +1,75 @@
+package cao.crawler.storm_crawler;
+/**
+ * Licensed to DigitalPebble Ltd under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * DigitalPebble licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.digitalpebble.storm.crawler.ConfigurableTopology;
+import com.digitalpebble.storm.crawler.Constants;
+import com.digitalpebble.storm.crawler.bolt.FetcherBolt;
+import com.digitalpebble.storm.crawler.bolt.JSoupParserBolt;
+import com.digitalpebble.storm.crawler.bolt.SiteMapParserBolt;
+import com.digitalpebble.storm.crawler.bolt.URLPartitionerBolt;
+import com.digitalpebble.storm.crawler.indexing.StdOutIndexer;
+//import com.digitalpebble.storm.crawler.persistence.StdOutStatusUpdater;
+import com.digitalpebble.storm.crawler.spout.MemorySpout;
+import com.digitalpebble.storm.crawler.sql.StatusUpdaterBolt;
+
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+
+/**
+ * Dummy topology to play with the spouts and bolts
+ */
+public class CrawlTopology extends ConfigurableTopology {
+
+    public static void main(String[] args) throws Exception {
+        ConfigurableTopology.start(new CrawlTopology(), args);
+    }
+
+    @Override
+    protected int run(String[] args) {
+        TopologyBuilder builder = new TopologyBuilder();
+
+        String[] testURLs = new String[] { "http://storm.apache.org", "http://stormcrawler.net/", "http://www.jd.com/"};
+
+        builder.setSpout("spout", new MemorySpout(testURLs));
+
+        builder.setBolt("partitioner", new URLPartitionerBolt())
+                .shuffleGrouping("spout");
+
+        builder.setBolt("fetch", new FetcherBolt()).fieldsGrouping(
+                "partitioner", new Fields("key"));
+
+        builder.setBolt("sitemap", new SiteMapParserBolt())
+                .localOrShuffleGrouping("fetch");
+
+        builder.setBolt("parse", new JSoupParserBolt()).localOrShuffleGrouping(
+                "sitemap");
+
+        builder.setBolt("index", new StdOutIndexer()).localOrShuffleGrouping(
+                "parse");
+
+        // can also use MemoryStatusUpdater for simple recursive crawls
+        //builder.setBolt("status", new StdOutStatusUpdater())
+        builder.setBolt("status", new StatusUpdaterBolt())
+                .localOrShuffleGrouping("fetch", Constants.StatusStreamName)
+                .localOrShuffleGrouping("sitemap", Constants.StatusStreamName)
+                .localOrShuffleGrouping("parse", Constants.StatusStreamName)
+                .localOrShuffleGrouping("index", Constants.StatusStreamName);
+
+        return submit("crawl", conf, builder);
+    }
+}
